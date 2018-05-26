8554567ed8452ebe159b0198f80db337271a795b
tungo_pc <tungo.ee.96@gmail.com>
2018-05-26 11:01:17 +0700

    test

--- web/touch.js
+++ web/touch.js
@@ -84,10 +84,10 @@ Ext.application({
                         console.log(Ext.getCmp('task'));
                         var someFunction = function() {
                             var task = new Ext.util.DelayedTask(function(task) {
-                                    console.log('a');
-                                    someFunction.call(task)
-                                },
-                                id: 'task');
+                                console.log('a');
+                                someFunction.call(task)
+                            });
+                            task.setId('task');
                             task.delay(1000);
                         };
                         someFunction();
