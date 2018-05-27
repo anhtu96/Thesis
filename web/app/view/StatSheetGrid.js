bfd5962a8712cc79918318474cef8f008cda0f5d
tungo_pc <tungo.ee.96@gmail.com>
2018-05-27 12:41:49 +0700

    test

--- web/app/view/StatSheetGrid.js
+++ web/app/view/StatSheetGrid.js
@@ -42,9 +42,8 @@ Ext.define('myApp.view.StatSheetGrid', {
             text: 'Name',
             dataIndex: 'devicename',
             flex: 1.5,
-            renderer: function(value, metaData, record, rowIndex, colIndex, store, view) {
-                console.log(record);
-                console.log(Ext.getCmp('sheetgrid').getSelectionModel().getCurrentPosition());
+            renderer: function(value, metaData) {
+                console.log(Ext.getCmp('sheetgrid'));
                 if (value == 'Sensor 1') {
                     value = '<font color="red">Sensor 1</font>'
                 }
